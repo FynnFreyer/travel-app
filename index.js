8b8a49b232d24656c891cd3b7e5658d4284c937a
Fynn Freyer <fynn.freyer@googlemail.com>
2021-12-19 20:30:07 +0100

    whoops

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const session = require('express-session')
-const session_options = require('./store/redis_session_options')
+const session_options = require('./store/session_options')
 const db = require('./store/db')
 const router = require('./routes')
 
