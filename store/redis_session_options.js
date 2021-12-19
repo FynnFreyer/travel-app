e49d959d372961d6ffd3c1ecfa3923c234e640c4
Fynn Freyer <fynn.freyer@googlemail.com>
2021-12-19 19:25:16 +0100

    sollte passen

--- store/redis_session_options.js
+++ store/redis_session_options.js
@@ -1,20 +1,26 @@
-// TODO filename is stupid
-const session = require("express-session");
-const connectRedis = require("connect-redis");
-const redis = require("redis");
+const session = require('express-session')
+const connectRedis = require('connect-redis')
+const redis = require('redis')
 
 const RedisStore = connectRedis(session)
 const redisClient = redis.createClient({
     url: process.env.REDIS_URL
 })
 
-module.exports = session({
+redisClient.on('error', (err) => {
+    console.log('Redis error: ', err);
+});
+
+session_options = {
     store: new RedisStore({client: redisClient}),
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
+    resave: false,
     cookie: {
         secure: false, // gets set true later if in production
         httpOnly: true,
         maxAge: 1000 * 60 * 15 // set max age to 1000 * 60 * 15 ms == 15 min
     }
-})
+}
+
+module.exports = session_options
