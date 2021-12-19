250e1aecebd9d283b225e739c6e530cb860769e4
Fynn Freyer <fynn.freyer@googlemail.com>
2021-12-19 19:56:14 +0100

    bitte?

--- store/db.js
+++ store/db.js
@@ -3,7 +3,10 @@ const knexfile = require('./knexfile')
 
 const db = knex({
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: false
+    }
 })
 
 module.exports = db
--- store/knexfile.js
+++ store/knexfile.js
@@ -28,7 +28,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+    },
     pool: {
       min: 2,
       max: 10
