d3a9a8266ec52dcc07b0ad55c78588fd3385f149
Fynn Freyer <fynn.freyer@googlemail.com>
2021-12-19 20:35:21 +0100

    ...

--- store/db.js
+++ store/db.js
@@ -5,10 +5,13 @@ let db_options = {
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
+        sslmode: 'require',
+        ssl: {rejectUnauthorized: false}
     }
 }
 
 if (process.env.IS_HEROKU) {
+    console.log('IS_HEROKU wurde gefunden')
     db_options.sslmode = 'require'
     db_options.ssl = {rejectUnauthorized: false}
 }
