799a645a3e4f0f685c67a3d7a81a3e8212b9165b
Fynn Freyer <fynn.freyer@googlemail.com>
2022-01-20 13:03:28 +0100

    add columns for email verification to db

---
+++ store/migrations/20220120114512_email_verification.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+    return knex.schema.alterTable('users', table => {
+        table.string('verification_token').notNullable()
+        table.boolean('verified').notNullable().defaultTo(false)
+    })
+}
+
+exports.down = function (knex) {
+    return knex.schema.alterTable('users', table => {
+        table.dropColumn('verification_token')
+        table.dropColumn('verified')
+    })
+}
