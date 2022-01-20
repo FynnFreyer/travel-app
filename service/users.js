5c20fe0d4f8c5be810a371fc20e9796ea033d631
Fynn Freyer <fynn.freyer@googlemail.com>
2022-01-20 18:28:40 +0100

    add verification method, add verification token on user creation and set verification status in session

--- service/users.js
+++ service/users.js
@@ -3,23 +3,34 @@ const pbkdf2 = require('pbkdf2')
 const crypto = require('crypto')
 
 // TODO can never change; bad
+//  should probably rather be stored in database
+//  for every individual user account,
+//  and controlled by env var, config file or something similar
 const iterations = 100000
 const keylen = 1024
 const digest = 'SHA256'
 
 class UsersService {
 
-    async createUser(email, password) {
-        // TODO validate email
+    async createUser(email, password, verification_token) {
         const {salt, pw_hash} = this.hashPassword(password)
 
         const [id] = await db.insert({
             email: email,
             salt: salt,
             pw_hash: pw_hash,
+            verification_token: verification_token
         })
             .into('users')
             .returning('id')
+
+        return id
+    }
+
+    async verifyUser(verification_token) {
+        const [id] = await db('users')
+            .where('verification_token', verification_token)
+            .update('verified', true, 'id')
 
         return id
     }
@@ -59,6 +70,18 @@ class UsersService {
                 let user = users[0]
                 const {id} = user
                 return id
+            })
+    }
+
+    async getVerifiedStatus(id) {
+        return await db
+            .select('verified')
+            .from('users')
+            .where('id', id)
+            .then((users) => {
+                let user = users[0]
+                const {verified} = user
+                return verified
             })
     }
 }
